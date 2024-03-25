312368071dab78ae185b120d328fe8ffa8552dc8
codehochuy <121989761+codehochuy@users.noreply.github.com>
2024-03-25 15:25:30 +0700

    base

---
+++ Video/MainWindow.xaml.cs
@@ -0,0 +1,97 @@
+﻿using Microsoft.Win32;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Windows.Threading;
+namespace Video
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        DispatcherTimer timer;
+        public MainWindow()
+        {
+            InitializeComponent();
+            timer = new DispatcherTimer();
+            timer.Interval = TimeSpan.FromSeconds(500);
+            timer.Tick += new EventHandler(Timer_Tick);
+        }
+
+        private void Timer_Tick(object? sender, EventArgs e)
+        {
+            Seek.Value = media.Position.TotalSeconds;
+        }
+
+        private void btnPlay_Click(object sender, RoutedEventArgs e)
+        {
+            media.Play(); 
+        }
+
+        private void btnPause_Click(object sender, RoutedEventArgs e)
+        {
+            media.Pause();
+        }
+
+        private void btnStop_Click(object sender, RoutedEventArgs e)
+        {
+            media.Stop();
+            Seek.Value = 0;
+        }
+
+        private void Vol_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
+        {
+            media.Volume = (double)Vol.Value;
+        }
+
+        private void Seek_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
+        {
+            if (media.NaturalDuration.HasTimeSpan)
+            {
+                media.Position = TimeSpan.FromSeconds(Seek.Value);
+            }
+        }
+
+        private void Window_Drop(object sender, DragEventArgs e)
+        {
+            string fileName = (string)((DataObject)e.Data).GetFileDropList()[0];
+            media.Source = new Uri(fileName);
+
+            media.LoadedBehavior = MediaState.Manual;
+            media.UnloadedBehavior = MediaState.Manual; 
+            media.Volume = (double)(Vol.Value);
+            media.Play();
+        }
+
+        private void media_MediaOpened(object sender, RoutedEventArgs e)
+        {
+            TimeSpan ts = media.NaturalDuration.TimeSpan;
+            Seek.Maximum = ts.TotalSeconds;
+            timer.Start();
+        }
+
+        private void btnSelectVideo_Click(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            openFileDialog.Filter = "Video files (*.mp4, *.avi)|*.mp4;*.avi|All files (*.*)|*.*";
+
+            if (openFileDialog.ShowDialog() == true)
+            {
+                string selectedFileName = openFileDialog.FileName;
+                media.Source = new Uri(selectedFileName);
+                media.LoadedBehavior = MediaState.Manual;
+                media.UnloadedBehavior = MediaState.Manual;
+                media.Volume = (double)Vol.Value;
+                media.Play();
+            }
+        }
+    }
+}
